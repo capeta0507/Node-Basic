f931c090ed341bd714a404bb13c4f8405cc74322
詹政勳 <nintendof1@gmail.com>
2019-06-22 22:02:38 +0800

    restful

---
+++ Express_restful/README.md
@@ -0,0 +1,24 @@
+# Express.js Route
+### RESTful /API 傳送參數的方法 :id , :NKey
+```javascript=
+// 路由/:參數
+// http://127.0.0.1:3000/API/123/OBI-159-app-874
+app.get('/api/:id/:Nkey',(req,res)=>{
+    // console.log('/api ...');
+    // 顯示網址
+    console.log(req.url);
+    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+    // 取得參數物件
+    let myReq = req.params;
+    // res.json(myReq); 
+    res.write('id : ' + myReq.id + '</br>');
+    res.write('nKey : ' + myReq.Nkey + '</br>');
+    res.end();
+})
+
+執行 http://127.0.0.1:3000/API/123/OBI-159-app-874
+說明 :
+>req.url 取得 /API/123/OBI-159-app-874 路徑
+>req.params 取得 /API 的參數 :id , :nKey
+>RESTful 概念
+```
---
+++ Express_restful/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '127.0.0.1';
+
+// App
+const app = express();
+
+// 允許跨網域 GET, POST, PUT, DELETE, OPTIONS 請求
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+// 不允許跨網域Request的話，可以關閉上面的 app.use()設定
+
+app.get('/', (req, res) => {
+    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+    res.write('Hello world , Node.js + Express.js </br>');
+    res.write('   1. Cross-Origin Resource Sharin (CORS) </br>');
+    res.write('   2. req.params拿到參數 </br>');
+    res.write('   3. req.query參數 </br>');
+    res.end();
+});
+// 路由
+app.get('/about',(req,res) => {
+    res.send('<h1>這是 About Page</h1>')
+})
+
+// 路由/:參數
+// http://127.0.0.1:3000/API/123/OBI-159-app-874
+app.get('/api/:id/:Nkey',(req,res)=>{
+    console.log('/api ...');
+    // 顯示網址 取得 /API/123/OBI-159-app-874 路徑
+    console.log(req.url);
+    // 取得參數的物件 取得 /API 的參數 :id , :nKey
+    let myReq = req.params;
+    // 以json傳送回參數
+    // res.json(myReq); 
+
+    // 以html方式傳送回去
+    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+    res.write('id : ' + myReq.id + '</br>');
+    res.write('nKey : ' + myReq.Nkey + '</br>');
+    res.end();
+})
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
