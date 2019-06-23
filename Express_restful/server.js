747d80fee05e92bf116aa202f04c33d7dea29e11
詹政勳 <nintendof1@gmail.com>
2019-06-23 12:00:21 +0800

    restful api get

--- Express_restful/server.js
+++ Express_restful/server.js
@@ -7,6 +7,9 @@ const HOST = '127.0.0.1';
 // App
 const app = express();
 
+// 提供靜態檔案顯示
+app.use('/', express.static(__dirname + '/wwwroot'));
+
 // 允許跨網域 GET, POST, PUT, DELETE, OPTIONS 請求
 app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin", "*");
@@ -16,14 +19,14 @@ app.use((req,res,next)=>{
 });
 // 不允許跨網域Request的話，可以關閉上面的 app.use()設定
 
-app.get('/', (req, res) => {
-    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
-    res.write('Hello world , Node.js + Express.js </br>');
-    res.write('   1. Cross-Origin Resource Sharin (CORS) </br>');
-    res.write('   2. req.params拿到參數 </br>');
-    res.write('   3. req.query參數 </br>');
-    res.end();
-});
+// app.get('/', (req, res) => {
+//     res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+//     res.write('Hello world , Node.js + Express.js </br>');
+//     res.write('   1. Cross-Origin Resource Sharin (CORS) </br>');
+//     res.write('   2. req.params拿到參數 </br>');
+//     res.write('   3. req.query參數 </br>');
+//     res.end();
+// });
 // 路由
 app.get('/about',(req,res) => {
     res.send('<h1>這是 About Page</h1>')
